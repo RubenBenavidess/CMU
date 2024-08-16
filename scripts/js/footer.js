d23783a03ad9f275c256654ce45bc5acd0498512
Joan Oswaldo Cobeña Zambrano <75241862+dr00p3r@users.noreply.github.com>
2024-08-16 10:02:51 -0500

    V0.0.4 Update footer adjust position
    now is called in resize and orientation change

--- scripts/js/footer.js
+++ scripts/js/footer.js
@@ -1,17 +1,23 @@
 // JavaScript Document
-window.addEventListener('load', function() {
+function adjustFooterPosition() {
     const contentHeight = document.body.scrollHeight;
     const viewportHeight = window.innerHeight;
     const footer = document.querySelector('footer');
     const footerHeight = footer.offsetHeight; 
 
-	console.log('contenido: ', (contentHeight + footerHeight));
-	console.log('viewport: ', viewportHeight);
+    console.log('contenido: ', (contentHeight + footerHeight));
+    console.log('viewport: ', viewportHeight);
     if ((contentHeight + footerHeight) >= viewportHeight) {
         footer.style.position = 'absolute';
-		footer.style.bottom = 'auto';
+        footer.style.bottom = 'auto';
     } else {
         footer.style.position = 'fixed';
         footer.style.bottom = '0';
     }
-});
+}
+
+window.addEventListener('load', adjustFooterPosition);
+
+window.addEventListener('resize', adjustFooterPosition);
+
+window.addEventListener('orientationchange', adjustFooterPosition);
